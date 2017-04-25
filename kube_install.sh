471c22fe5a976a7c09fd7d932017f85bf8e96036
Bruno Maciel massaini <bruno.massaini@C02RV1XFG8WL.fritz.box>
2017-04-25 22:02:45 +0200

    install

--- kube_install.sh
+++ kube_install.sh
@@ -22,15 +22,21 @@ BASEURL="http://cbs.centos.org/repos/"$RELEASE"/x86_64/os/"
 GPGCHECK="0"
 
 # Installation
-echo "Common Packages and Dependencies"
+if [ $ROLE = "controller" ]; then
+  echo "Configuring CONTROLLER Role"
+else
+  echo "Configuring MINION Role"
+done
 
-echo "- Installing and configuring NTP"
+echo "- Common Packages and Dependencies"
+
+echo "-- Installing and configuring NTP"
 {
   yum install -y ntp
   systemctl enable ntpd && systemctl start ntpd
 } &> /dev/null
 
-echo "- Configuring Repo"
+echo "-- Configuring Repo"
 if [ -f $REPO_FILE ]; then
    > $REPO_FILE
 fi
@@ -40,10 +46,10 @@ echo baseurl=$BASEURL >> $REPO_FILE
 echo gpgcheck=$GPGCHECK >> $REPO_FILE
 yum update &> /dev/null
 
-echo "- Installing Kube and Docker"
+echo "-- Installing Kube and Docker"
 yum install -y --enablerepo=$RELEASE kubernetes docker &> /dev/null
 
-echo "- Configuring etc/hosts"
+echo "-- Configuring etc/hosts"
 sed -i '/$MASTER_DNS/d' /etc/hosts
 sed -i '/$MINIONS_DNS/d' /etc/hosts
 echo $MASTER_IP $MASTER_DNS >> /etc/hosts
@@ -52,26 +58,25 @@ for i in ${!MINIONS_IP[@]} ; do
 done
 
 if [ $ROLE = "controller" ]; then
-  echo "Configuring CONTROLLER Role"
 
-  echo "- Kubernetes/config"
+  echo "-- Kubernetes/config"
   sed -i '/KUBE_MASTER/c\KUBE_MASTER="--master=http://'"${MASTER_DNS}"':8080"' /etc/kubernetes/config
   echo 'KUBE_ETCD_SERVERS="--etcd-servers=http://'"${MASTER_DNS}"':2379"' >> /etc/kubernetes/config
-  
-  echo "- Kubernetes/apiserver"
+
+  echo "-- Kubernetes/apiserver"
   sed -i '/KUBE_API_ADDRESS/c\KUBE_API_ADDRESS="--address=0.0.0.0"' /etc/kubernetes/apiserver
   sed -i '/KUBELET_PORT/c\KUBELET_PORT="--kubelet-port=10250"' /etc/kubernetes/apiserver
   sed -i '/KUBE_ADMISSION_CONTROL/d' /etc/kubernetes/apiserver
-  
-  echo "- Installing and Configuring ETCD"
+
+  echo "-- Installing and Configuring ETCD"
   yum -y install etcd &> /dev/null
   sed -i '/ETCD_LISTEN_CLIENT_URLS/c\ETCD_LISTEN_CLIENT_URLS="http://0.0.0.0:2379"' /etc/etcd/etcd.conf
   sed -i '/ETCD_ADVERTISE_CLIENT_URLS/c\ETCD_ADVERTISE_CLIENT_URLS="http://0.0.0.0:2379"' /etc/etcd/etcd.conf
-  
+
   echo "* Starting 4 Services *"
   systemctl enable etcd kube-apiserver kube-controller-manager kube-scheduler &> /dev/null
   systemctl start etcd kube-apiserver kube-controller-manager kube-scheduler
-  echo "*" `systemctl status etcd kube-apiserver kube-controller-manager kube-scheduler | grep "(running)" | wc -l` "Services Started *" 
+  echo "*" `systemctl status etcd kube-apiserver kube-controller-manager kube-scheduler | grep "(running)" | wc -l` "Services Started *"
 
   echo "Configuring MINION Role"
   for i in ${!MINIONS_IP[@]} ; do
@@ -79,6 +84,8 @@ if [ $ROLE = "controller" ]; then
     echo "-- "
     ssh -i $SSHKEY $SSHUSER@$MINIONS_DNS`expr $i + 1` << EOF
       echo "I was here" >> ~/teste
+      echo "TESTE"
+      sudo echo "SUDO HERE" >> ~/teste_sudo
 EOF
   done
 
